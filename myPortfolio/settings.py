65ff7ebe7f000b349a378a86c36f0e0a71f40e2d
Aodan <hardy-a4@ulster.ac.uk>
2024-08-28 14:10:18 +0100

    didint work, slight change

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'pictures')
+STATIC_ROOT = os.path.join(BASE_DIR, 'mySite/static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures/projectImages')
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/pictures/'
